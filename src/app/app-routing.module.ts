4d86ed7618391fbec3dd4f566db3ee3cebcacd68
henryaf0519 <henryaf0519@gmail.com>
2026-04-14 10:56:47 -0500

    route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/products', pathMatch: 'full' },
-  
+  { path: '', redirectTo: '/products', pathMatch: 'full' },  
   { path: '**', redirectTo: '/products' } 
 ];
 
