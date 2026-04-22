6d50bb8d047ebb72d437715f26266fc041f69027
henryaf0519 <henryaf0519@gmail.com>
2026-04-22 15:52:39 -0500

    feat(insurance-calculator): enhance component with script loading and update template structure

--- src/app/shared/components/insurance-calculator/insurance-calculator.component.ts
+++ src/app/shared/components/insurance-calculator/insurance-calculator.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-insurance-calculator',
   standalone: true,
   imports: [],
   templateUrl: './insurance-calculator.component.html',
-  styleUrl: './insurance-calculator.component.scss'
+  styleUrl: './insurance-calculator.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class InsuranceCalculatorComponent {
+
+  ngOnInit(): void {
+    this.loadArriendyScript();
+  }
+
+  private loadArriendyScript(): void {
+    const scriptId = 'arriendy-widget-script';
+    
+    // Evitamos cargar el script más de una vez si el componente se destruye y recrea
+    if (!document.getElementById(scriptId)) {
+      const script = document.createElement('script');
+      script.id = scriptId;
+      script.src = 'https://cdn.arriendy.paisasoft.com/quoter/v1/widget.js';
+      script.defer = true;
+      document.body.appendChild(script);
+    }
+  }
 
 }
